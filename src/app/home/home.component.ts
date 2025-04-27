66a0371d00330c59492a7e105786dccf5c5f87e0
Akasha Sahu <akashasahu2001@gmail.com>
2025-04-27 14:12:15 +0530

    home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  books = [
+    {
+      img: '/assets/atomic-habit.png',
+      title: 'Atomic Habit'
+    },
+    {
+      img: '/assets/psy-money.png',
+      title: 'The Psychology of Money'
+    }
+  ]
 }
