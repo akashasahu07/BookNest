d184ab2bcf98bdcae5802add476ef67fc792d1e2
Akasha Sahu <akashasahu2001@gmail.com>
2025-05-08 12:19:23 +0530

    home update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +11,8 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router){}
+
   books = [
     {
       img: '/assets/images/atomic-habit.png',
@@ -20,10 +25,30 @@ export class HomeComponent {
       book: '/assets/pdfs/Psychology-of-Money.pdf'
     },
     {
-      img: '/assets/images/atomic-habit.png',
-      title: 'Grow More'
+      img: '/assets/images/rich-dad-poor-dad.png',
+      title: 'Rich Dad Poor Dad',
+      book: '/assets/pdfs/Rich-Dad-Poor-Dad.pdf'
+    },
+    {
+      img: '/assets/images/the-subtle-art.png',
+      title: 'The Subtle Art of Not Giving a F*ck',
+      book: '/assets/pdfs/the-subtle-art.pdf'
+    },
+    {
+      img: '/assets/images/wings-of-fire.png',
+      title: 'Wings Of Fire by APJ Abdul Kalam',
+      book: '/assets/pdfs/wings-of-fire.pdf'
     }
   ]
-}
 
-// time 48
+  onLogout() {
+    // const auth = getAuth();
+    signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+
+    this.router.navigate(['/login'])
+  }
+}
